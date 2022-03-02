500fd926d00837bb813a294e264242316eb0eb53
expoli <me@expoli.tech>
2022-03-02 18:22:29 +0800

    练习 1.12： 修改Lissajour服务，从URL读取变量，比如你可以访问 http://localhost:8000/?cycles=20 这个URL，这样访问可以将程序里的cycles默认的5修改为20。字符串转换为数字可以调用strconv.Atoi函数。你可以在godoc里查看strconv.Atoi的详细说明。

---
+++ ch1/Exercise/1.12/1.12.go
@@ -0,0 +1,83 @@
+// Lissajous generates GIF animations of random Lissajous figures.
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"log"
+	"math"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"sync"
+	"time"
+)
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+var mutex sync.Mutex
+
+func main() {
+	// The sequence of images is deterministic unless we seed
+	// the pseudo-random number generator using the current time.
+	// Thanks to Randall McPherson for pointing out the omission.
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		lissajous(w, cyclesHandler(w, r))
+	}
+
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+
+}
+
+func cyclesHandler(w http.ResponseWriter, r *http.Request) int {
+	cycles := r.Form.Get("cycles")
+	if len(cycles) != 0 {
+		result, err := strconv.Atoi(cycles)
+		if err != nil {
+			fmt.Sprintf("error: %s", err)
+			return 5
+		}
+		if result <= 0 {
+			return 5
+		}
+		return result
+	}
+	return 5
+}
+
+func lissajous(w http.ResponseWriter, cycles int) {
+	const (
+		res     = 0.001 // angular resolution
+		size    = 100   // image canvas covers [-size..+size]
+		nframes = 64    // number of animation frames
+		delay   = 8     // delay between frames in 10ms units
+	)
+
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(w, &anim) // NOTE: ignoring encoding errors
+}
