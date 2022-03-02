e423d1454a9f22156b136642617d4bc445644c4a
expoli <me@expoli.tech>
2022-03-02 10:19:47 +0800

    练习 1.4： 修改dup2，出现重复的行时打印文件名称。

---
+++ ch1/Exercise/1.4.go
@@ -0,0 +1,47 @@
+// Dup2 prints the count and text of lines that appear more than once
+// in the input.  It reads from stdin or from a list of named files.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			file_count := make(map[string]int)
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, file_count)
+			f.Close()
+			for line, n := range file_count {
+				if n > 1 {
+					fmt.Printf("%s\t%d\t%s\n", arg, n, line)
+				}
+			}
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%s\t%d\t%s\n", "stdin", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+}
