749bdb26c4e2eaba4ec853ffc0b774cf3ad662fa
expoli <me@expoli.tech>
2022-03-05 20:28:39 +0800

    一个获取链接的库

---
+++ ch5/links/links.go
@@ -0,0 +1,76 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 138.
+//!+Extract
+
+// Package links provides a link-extraction function.
+// 新的匿名函数被引入，用于替换原来的visit函数。
+// 该匿名函数负责将新连接添加到切片中。
+// 在Extract中，使用forEachNode遍历HTML页面，
+// 由于Extract只需要在遍历结点前操作结点，
+// 所以forEachNode的post参数被传入nil。
+package links
+
+import (
+	"fmt"
+	"net/http"
+
+	"golang.org/x/net/html"
+)
+
+// Extract makes an HTTP GET request to the specified URL, parses
+// the response as HTML, and returns the links in the HTML document.
+// 导出函数（Public）
+func Extract(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+
+	var links []string
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key != "href" {
+					continue
+				}
+				// 现在links中存储的不是href属性的原始值，而是通过resp.Request.URL解析后的值。
+				// 解析后，这些连接以绝对路径的形式存在，可以直接被http.Get访问。
+				link, err := resp.Request.URL.Parse(a.Val)
+				if err != nil {
+					continue // ignore bad URLs
+				}
+				links = append(links, link.String())
+			}
+		}
+	}
+	forEachNode(doc, visitNode, nil)
+	return links, nil
+}
+
+//!-Extract
+
+// Copied from gopl.io/ch5/outline2.
+// 内部函数（只能内部使用）
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
