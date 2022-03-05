72b29dbc47660e9d3ee453295e3f56e655f8181d
expoli <me@expoli.tech>
2022-03-05 21:03:19 +0800

    获取并显示HTML页面的标题，当需要处理的错误多起来之后，可能会导致错误处理无法完整地正确处理

---
+++ ch5/title1/title.go
@@ -0,0 +1,85 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 144.
+
+// Title1 prints the title of an HTML document specified by a URL.
+package main
+
+/*
+//!+output
+$ go build gopl.io/ch5/title1
+$ ./title1 http://gopl.io
+The Go Programming Language
+$ ./title1 https://golang.org/doc/effective_go.html
+Effective Go - The Go Programming Language
+$ ./title1 https://golang.org/doc/gopher/frontpage.png
+title: https://golang.org/doc/gopher/frontpage.png
+    has type image/png, not text/html
+//!-output
+*/
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+// Copied from gopl.io/ch5/outline2.
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
+
+//!+
+func title(url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	// Check Content-Type is HTML (e.g., "text/html; charset=utf-8").
+	ct := resp.Header.Get("Content-Type")
+	if ct != "text/html" && !strings.HasPrefix(ct, "text/html;") {
+		// resp.Body.close调用了多次，这是为了确保title在所有执行路径下（即使函数运行失败）都关闭了网络连接。
+		// 随着函数变得复杂，需要处理的错误也变多，维护清理逻辑变得越来越困难。
+		// 而Go语言独有的defer机制可以让事情变得简单。
+		resp.Body.Close()
+		return fmt.Errorf("%s has type %s, not text/html", url, ct)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" &&
+			n.FirstChild != nil {
+			fmt.Println(n.FirstChild.Data)
+		}
+	}
+	forEachNode(doc, visitNode, nil)
+	return nil
+}
+
+//!-
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		if err := title(arg); err != nil {
+			fmt.Fprintf(os.Stderr, "title: %v\n", err)
+		}
+	}
+}
