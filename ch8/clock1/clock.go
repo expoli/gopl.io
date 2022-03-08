2fe0a6855bb9ecb14ebaf92ce605c132aab7a207
expoli <me@expoli.tech>
2022-03-08 11:35:18 +0800

    普通的无并发功能的时间服务器

---
+++ ch8/clock1/clock.go
@@ -0,0 +1,46 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 219.
+//!+
+
+// Clock1 is a TCP server that periodically writes the time.
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		/*
+			调用 Accept 之后，会直接阻塞，直到一个新的连接被创建，然后会返回一个net.Conn对象来表示这个连接。
+		*/
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		handleConn(conn) // handle one connection at a time
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	for {
+		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		if err != nil {
+			return // e.g., client disconnected
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+//!-
