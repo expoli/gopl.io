b541f39598532257ce8bc2fc38182cd42ac06965
expoli <me@expoli.tech>
2022-03-09 10:50:00 +0800

    无协程数量限制，可能会在高峰期创建成百上千的goroutine，导致打开文件数量达到限制

---
+++ ch8/du3/du.go
@@ -0,0 +1,127 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 250.
+
+// The du3 command computes the disk usage of the files in a directory.
+package main
+
+// The du3 variant traverses all directories in parallel.
+// It uses a concurrency-limiting counting semaphore
+// to avoid opening too many files at once.
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+var vFlag = flag.Bool("v", false, "show verbose progress messages")
+
+//!+
+func main() {
+	// ...determine roots...
+
+	//!-
+	flag.Parse()
+
+	// Determine the initial directories.
+	roots := flag.Args()
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+
+	//!+
+	// Traverse each root of the file tree in parallel.
+	fileSizes := make(chan int64)
+	// 因为不知道具体的工作量大小，所以需要使用一个 waitGroup 信号量，进行通道的关闭处理
+	// 它使用sync.WaitGroup（§8.5）来对仍旧活跃的walkDir调用进行计数，
+	// 另一个goroutine会在计数器减为零的时候将fileSizes这个channel关闭。
+	var n sync.WaitGroup
+	for _, root := range roots {
+		// 协程 ++
+		n.Add(1)
+		// n 进行指针赋值，保证操作的都是同一个 n
+		go walkDir(root, &n, fileSizes)
+	}
+	go func() {
+		// 当所有的工作负载都完成的时候，此协程负责关闭相应的通道，负责通知其他协程工作已完成
+		n.Wait()
+		close(fileSizes)
+	}()
+	//!-
+
+	// Print the results periodically.
+	// 主协程负责定时打印进度状况
+	var tick <-chan time.Time
+	if *vFlag {
+		tick = time.Tick(500 * time.Millisecond)
+	}
+	var nfiles, nbytes int64
+loop:
+	for {
+		select {
+		case size, ok := <-fileSizes:
+			if !ok {
+				// 标签打断
+				break loop // fileSizes was closed
+			}
+			nfiles++
+			nbytes += size
+		case <-tick:
+			printDiskUsage(nfiles, nbytes)
+		}
+	}
+
+	printDiskUsage(nfiles, nbytes) // final totals
+	//!+
+	// ...select loop...
+}
+
+//!-
+
+func printDiskUsage(nfiles, nbytes int64) {
+	fmt.Printf("%d files  %.1f GB\n", nfiles, float64(nbytes)/1e9)
+}
+
+// walkDir recursively walks the file tree rooted at dir
+// and sends the size of each found file on fileSizes.
+//!+walkDir
+func walkDir(dir string, n *sync.WaitGroup, fileSizes chan<- int64) {
+	// 函数返回的时候，进行信号量的完成处理
+	defer n.Done()
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			n.Add(1)
+			subdir := filepath.Join(dir, entry.Name())
+			go walkDir(subdir, n, fileSizes)
+		} else {
+			fileSizes <- entry.Size()
+		}
+	}
+}
+
+//!-walkDir
+
+//!+sema
+// sema is a counting semaphore for limiting concurrency in dirents.
+var sema = make(chan struct{}, 20)
+
+// dirents returns the entries of directory dir.
+func dirents(dir string) []os.FileInfo {
+	sema <- struct{}{}        // acquire token
+	defer func() { <-sema }() // release token
+	// ...
+	//!-sema
+
+	entries, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du: %v\n", err)
+		return nil
+	}
+	return entries
+}
