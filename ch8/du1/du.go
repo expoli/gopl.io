321fff5e66f3021a1626d01514eaf9042c0b937f
expoli <me@expoli.tech>
2022-03-09 10:29:06 +0800

    使用一个协程去处理遍历文件和统计大小信息

---
+++ ch8/du1/du.go
@@ -0,0 +1,92 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 247.
+
+//!+main
+
+// The du1 command computes the disk usage of the files in a directory.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	// Determine the initial directories.
+	flag.Parse()
+	roots := flag.Args()
+	if len(roots) == 0 {
+		roots = []string{"."}
+	}
+
+	// Traverse the file tree.
+	fileSizes := make(chan int64)
+	/*
+		一个协程去处理遍历文件和统计大小信息
+		效率一般
+	*/
+	go func() {
+		for _, root := range roots {
+			walkDir(root, fileSizes)
+		}
+		close(fileSizes)
+	}()
+
+	// Print the results.
+	var nfiles, nbytes int64
+	for size := range fileSizes {
+		nfiles++
+		nbytes += size
+	}
+	printDiskUsage(nfiles, nbytes)
+}
+
+func printDiskUsage(nfiles, nbytes int64) {
+	fmt.Printf("%d files  %.1f GB\n", nfiles, float64(nbytes)/1e9)
+}
+
+//!-main
+
+//!+walkDir
+
+// walkDir recursively walks the file tree rooted at dir
+// and sends the size of each found file on fileSizes.
+func walkDir(dir string, fileSizes chan<- int64) {
+	for _, entry := range dirents(dir) {
+		if entry.IsDir() {
+			subdir := filepath.Join(dir, entry.Name())
+			walkDir(subdir, fileSizes)
+		} else {
+			fileSizes <- entry.Size()
+		}
+	}
+}
+
+// dirents returns the entries of directory dir.
+/*
+ioutil.ReadDir函数会返回一个os.FileInfo类型的slice，
+os.FileInfo类型也是os.Stat这个函数的返回值。
+对每一个子目录而言，walkDir会递归地调用其自身，
+同时也在递归里获取每一个文件的信息。
+
+walkDir函数会向fileSizes这个channel发送一条消息。
+这条消息包含了文件的字节大小。
+*/
+func dirents(dir string) []os.FileInfo {
+	entries, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "du1: %v\n", err)
+		return nil
+	}
+	return entries
+}
+
+//!-walkDir
+
+// The du1 variant uses two goroutines and
+// prints the total after every file is found.
