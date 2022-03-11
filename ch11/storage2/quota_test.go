35b04692f382a7f3a7141459e1a02f33304b2bdf
expoli <me@expoli.tech>
2022-03-11 20:55:44 +0800

    1. 使用 defer 用来恢复暂存的全局变量 2. 这种处理方式是安全的，因为 go test命令并不会同时并发地执行多个测试。

--- ch11/storage2/quota_test.go
+++ ch11/storage2/quota_test.go
@@ -11,6 +11,25 @@ import (
 
 func TestCheckQuotaNotifiesUser(t *testing.T) {
 	var notifiedUser, notifiedMsg string
+
+	/*
+		我们必须修改测试代码恢复notifyUsers原先的状态以便后续其他的测试没有影响，
+		要确保所有的执行路径后都能恢复，包括测试失败或panic异常的情形。
+
+		在这种情况下，我们建议使用defer语句来延后执行处理恢复的代码。
+
+		这种处理模式可以用来暂时保存和恢复所有的全局变量，
+		包括命令行标志参数、调试选项和优化参数；
+		安装和移除导致生产代码产生一些调试信息的钩子函数；
+		还有有些诱导生产代码进入某些重要状态的改变，
+		比如超时、错误，甚至是一些刻意制造的并发行为等因素。
+
+		以这种方式使用全局变量是安全的，
+		因为go test命令并不会同时并发地执行多个测试。
+	*/
+	saved := notifyUser
+	defer func() { notifyUser = saved }()
+
 	/*
 		伪邮件发送函数，覆盖 CheckQuota 中的同名函数
 	*/
